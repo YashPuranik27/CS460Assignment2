1d61451144f5b75eb4595ad80ff3b52d34afac4c
DerpySlurpee <101681679+DerpySlurpee@users.noreply.github.com>
2023-11-14 02:03:37 -0500

    code to implement RRT

--- car_2.py
+++ car_2.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from matplotlib.transforms import Affine2D
-import sys, getopt
 from create_scene import create_plot, add_polygon_to_scene, load_polygons, show_scene
+import random
+from math import pi, radians, floor, sqrt
+from planar_car import PlanarCar
+import argparse
 
 # Initial state [x, y, theta]
 init_x = 1
@@ -35,184 +36,165 @@ tireAngle=0
 
 tireOffest=0.5
 
-def differential_drive_model(q, u):
-    dq = np.zeros_like(q)
 
-    # q[2] is theta(t)
-    # u[0] is velocity
-    # u[1] is steeringAngle
-
-    dq[0] = u[0] * np.cos(q[2]) * dt # v * cos(theta(t)
-    dq[1] = u[0] * np.sin(q[2]) * dt # v * sin(theta(t)
-    dq[2] = u[0]/length * np.tan(u[1]) * dt  # v/L*tan(steeringAngle)
-
-    return dq
-
-
-def on_key(event):
-    global u
-    global tireAngle
-
-    omega_step = np.pi/30
-    v_step = 0.05
-
-
-    if event.key == 'up':
-        u[0] = np.clip(u[0] + v_step, v_min, v_max)
-        #tireAngle = 0
-
-    elif event.key == 'down':
-        u[0] = np.clip(u[0] - v_step, v_min, v_max)
-        #tireAngle = 0
-
-    elif event.key == 'right':
-
-        if u[1] < 0 : #car was steering left, turn it
-            u[1] = 0
-            tireAngle = np.pi / 30 * -1
-        else:
-            u[1] = np.clip(u[1] + omega_step, omega_min, omega_max)
-            tireAngle = np.pi / 30
-
-        #if float(u[0]) == float(0.0):
-            #u[0] = np.clip(u[0] + v_step, v_min, v_max) # if the car stopped, move it
-
-    elif event.key == 'left':
-        u[1] = np.clip(u[1] - omega_step, omega_min, omega_max)
-        if u[1] > 0:
-            u[1] = 0
-            tireAngle = np.pi / 30
-        else:
-            u[1] = np.clip(u[1] - omega_step, omega_min, omega_max)
-            tireAngle = np.pi / 30 * -1
-
-        #if float(u[0]) == float(0.0):
-            #u[0] = np.clip(u[0] - v_step, v_min, v_max) # if the car stopped, move it
+# node for RRT search tree
+class treeNode:
+    def __init__(self, x, y):
+        self.x, self.y, self.children, self.parent = x, y, [], None
 
 
 
-def draw_rotated_tire(ax, center, width, height, angle_degrees, color='b'):
+# RRT Claass ===============================================================
+class RRT():
 
-    x, y = center
-    t = Affine2D().rotate_deg_around(x, y, angle_degrees) + ax.transData
+    def __init__(self, start, goal, numIterations, grid, stepSize=0.2):
+        self.randomTree, self.goal = treeNode(*start), treeNode(*goal)
+        self.iterations, self.rho = min(numIterations, 1000), stepSize
+        self.path_distance, self.nearestDist, self.numWaypoints = 0, float('inf'), 0
+        self.nearestNode, self.Waypoints = None, []
 
-    xF1 = x + (tireOffest * width)
-    yF1 = y + (tireOffest * height)
-    tireFront = patches.Rectangle((xF1 - 0.025, yF1 - 0.01), 0.05, 0.02, linewidth=1, edgecolor='red',
-                                  facecolor='red')
-    tireFront.set_transform(t)
-    ax.add_patch(tireFront)
+    #plan a path from start to end points
+    def plan(self, start, end):
+        pass
 
-    xF2 = x + (tireOffest * width)
-    yF2 = y - (tireOffest * height)
-    tireFront = patches.Rectangle((xF2 - 0.025, yF2 - 0.01), 0.05, 0.02, linewidth=1, edgecolor='red',
-                                  facecolor='red')
-    tireFront.set_transform(t)
-    ax.add_patch(tireFront)
+    #sample a random point (within the grid)
+    def samplePoint(self, goal):
+        return np.array(goal if random.random() < 0.05 else [random.uniform(-pi, pi) for _ in range(2)])
+
+    #add to the nearest node
+    def addChild(self, x, y):
+        newNode = self.goal if (x, y) == (self.goal.x, self.goal.y) else treeNode(x, y) # if goal, add it, else , add the node
+        self.nearestNode.children.append(newNode)
+        newNode.parent = self.nearestNode
+
+    def goToPoint(self, start, end):
+        return np.array([start.x, start.y]) + self.rho * self.unitVector(start, end)
+
+    #check if hit obstacle between the start and the end points
+    def isInObstacle(self, start, end, car):
+        u_hat = self.unitVector(start, end)
+        for i in range(floor(self.rho / radians(5))):
+            test = [start.x + i * u_hat[0], start.y + i * u_hat[1]]
+            #car.theta1, car.theta2 = test
+            #car.re_orient()
+            #if any(car.check_arm_collisions()):
+            #    return True
+        return False
+
+    def unitVector(self, start, end):
+        v = np.array([end[0] - start.x, end[1] - start.y])
+        return v / np.linalg.norm(v)
+
+    #find the nearest node from the point
+    def findNearest(self, root, point):
+        if root:
+            dist = self.distance(root, point)
+            if dist < self.nearestDist:
+                self.nearestNode, self.nearestDist = root, dist
+            for child in root.children:
+                self.findNearest(child, point)
+
+    #calculate the distance between the node and the point
+    def distance(self, node, point):
+        return sqrt((node.x - point[0]) ** 2 + (node.y - point[1]) ** 2)
+
+    #check if the goal is reached
+    def goalFound(self, point):
+        return self.distance(self.goal, point) <= self.rho
+
+    def resetNearestValues(self):
+        self.nearestNode, self.nearestDist = None, float('inf')
+
+    #find the path between start and goal
+    def retracePath(self, goal):
+        if (goal.x, goal.y) != (self.randomTree.x, self.randomTree.y):
+            self.numWaypoints += 1
+            self.Waypoints.insert(0, np.array([goal.x, goal.y]))
+            self.path_distance += self.rho
+            self.retracePath(goal.parent)
 
 
-def draw_rotated_rectangle(ax, center, width, height, angle_degrees, color='b'):
+# End of RRT Claass =========================================================
 
-    global tireAngle
-
-
-    x, y = center
-    rect = patches.Rectangle((x - width / 2, y - height / 2), width, height, linewidth=1, edgecolor=color,
-                             facecolor='none')
-    t = Affine2D().rotate_deg_around(x, y, angle_degrees) + ax.transData
-    rect.set_transform(t)
-    ax.add_patch(rect)
-
-    # draw Rear Tire
-    xR1 = x - (tireOffest * width)
-    yR1 = y + (tireOffest * height)
-    tireRear = patches.Rectangle((xR1 - 0.025, yR1 - 0.01), 0.05, 0.02, linewidth=1, edgecolor='red',
-                                 facecolor='none')
-    tireRear.set_transform(t)
-    ax.add_patch(tireRear)
-
-    xR2 = x - (tireOffest * width)
-    yR2 = y - (tireOffest * height)
-    tireRear = patches.Rectangle((xR2 - 0.025, yR2 - 0.01), 0.05, 0.02, linewidth=1, edgecolor='red',
-                                 facecolor='none')
-    tireRear.set_transform(t)
-    ax.add_patch(tireRear)
-
-
-
-#read input control
-
-
-def main(argv):
+def rrt_tree(start, goal, car, map):
 
     global q
-    global u
-    init_v = 0.4
-    init_o = 0.5
-    u = np.array([init_v, init_o])
 
-    msg1="\nno input constant control, default v = "+str(init_v)+" omaga = "+str(init_o)
-    msg2="\ninvlaid input parmamerts, default v = "+str(init_v)+" omaga = "+str(init_o)
-    defaultMsg = msg1
+    print(start)
+    print(goal)
 
-    if len(argv) < 3:
-        print(msg1)
+    plt.close('all')
+    fig, ax = plt.subplots()
+    ax.plot(start[0], start[1], 'ro')
+    ax.set_xlim(-pi, pi)
+    ax.set_ylim(-pi, pi)
+    #plt.xlim(0, 2)
+    #plt.ylim(0, 2)
+
+    [add_polygon_to_scene(poly, ax, True) for poly in poly_map]
+    dq = car.differential_drive_model(q, u)
+    q += dq
+    car.draw_rotated_tire(ax, [q[0], q[1]], length, width, np.degrees(q[2] + tireAngle))
+    car.draw_rotated_rectangle(ax, [q[0], q[1]], length, width, np.degrees(q[2]))
+
+
+    rrt = RRT(start, goal, 1000, radians(5))
+
+    for i in range(1000):
+        rrt.resetNearestValues()
+        point = rrt.samplePoint(goal)
+        rrt.findNearest(rrt.randomTree, point)
+        new_point = rrt.goToPoint(rrt.nearestNode, point)
+
+        if not rrt.isInObstacle(rrt.nearestNode, new_point, car):
+            rrt.addChild(*new_point)
+            ax.plot([rrt.nearestNode.x, new_point[0]], [rrt.nearestNode.y, new_point[1]], 'go', linestyle="--")
+            plt.pause(0.01)
+
+            if rrt.goalFound(new_point):
+                rrt.addChild(*goal)
+                print("Goal Found")
+                break
+        elif not i:
+            print("Error: Start node in obstacle")
+            break
     else:
-        if argv[0] == "--control":
-            init_v=float(argv[1])
-            init_o=float(argv[2])
-            print(init_v, init_o)
-        else:
-            print(msg2)
+        plt.pause(1)
 
-    print("initial x = " + str(init_x) + " y = " + str(init_y) + "\n")
+    rrt.retracePath(rrt.goal)
+    rrt.Waypoints.insert(0, start)
+    waypoint_pairs = zip(rrt.Waypoints[:-1], rrt.Waypoints[1:])
+
+    for (wp_start, wp_end) in waypoint_pairs:
+        ax.plot([wp_start[0], wp_end[0]], [wp_start[1], wp_end[1]], 'ro', linestyle="--")
+
+    plt.pause(1)
+    #plt.close()
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="RRT planning with ddynamics.")
+    parser.add_argument('--start', type=float, nargs=3, required=True, help='Start orientation')
+    parser.add_argument('--goal', type=float, nargs=3, required=True, help='Target orientation')
+    parser.add_argument('--map', required=True, help='Map file with polygons')
+    args = parser.parse_args()
+
+    # Directly use modified angles and load polygons for the arm controller
+    car = PlanarCar()
+    #car.set_arm_obs(load_polygons(args.map))
 
     # Initialize plot
-    fig, ax = plt.subplots(figsize=(6, 6))
-    #fig.canvas.mpl_connect('key_press_event', on_key)
+    #fig, ax = plt.subplots(figsize=(6, 6))
+    poly_map = load_polygons(args.map)
+    #plt.clf()
+    #ax = plt.gca()
+    #plt.xlim(0, 2)
+    #plt.ylim(0, 2)
 
-    poly_map = load_polygons("rigid_polygons.npy")
-
-
-    omega_step = np.pi / 15
-    v_step = 0.1
-    u = np.array([init_v, init_o])
-    tireAngle = np.pi / 30
+    #show_scene(ax)
 
 
-    x_max = 2
-    y_max = 2
-
-    #ax = create_plot()
-    plt.clf()
-    ax = plt.gca()
-    plt.xlim(0, x_max)
-    plt.ylim(0, y_max)
-    [add_polygon_to_scene(poly, ax, True) for poly in poly_map]
-
-    while True:
-        # Update state
-        dq = differential_drive_model(q, u)
-        q += dq
-
-        # Visualization
-
-
-        if (q[0]<x_max and q[1]<y_max and q[0]>0 and q[1]>0 ):
-            # Draw Front Tire
-            draw_rotated_tire(ax, [q[0], q[1]], length, width, np.degrees(q[2] + tireAngle))
-
-            # Draw robot body
-            draw_rotated_rectangle(ax, [q[0], q[1]], length, width, np.degrees(q[2]))
-
-            show_scene(ax)
-            plt.pause(0.05)
-
-        else:
-            #move the car to initial state
-            u = np.array([init_v, init_o])
-            q = init_q
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+    # Start the RTT tree process
+    rrt_tree((args.start[0],args.start[1]), (args.goal[0],args.goal[1]), car, poly_map)
