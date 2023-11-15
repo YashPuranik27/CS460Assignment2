8710d29d288d537cd9ab69be0562b28c98faabf4
Yash <50245579+YashPuranik27@users.noreply.github.com>
2023-11-14 21:47:38 -0500

    Update rigid_body_4.py

--- rigid_body_4.py
+++ rigid_body_4.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class treeNode:
+class tree:
     def __init__(self, x, y, theta):
         self.x = x  # x coord
         self.y = y  # ycoord
@@ -18,10 +18,10 @@ class treeNode:
         self.parent = None
 
 
-class RTT:
+class rotate:
     def __init__(self, start, goal, iterations, stepSize):
-        self.randomTree = treeNode(*start)
-        self.goal = treeNode(*goal)
+        self.randomTree = tree(*start)
+        self.goal = tree(*goal)
         self.nearestNode = None
         self.currentNode = None
         self.iterations = min(iterations, 1000)
@@ -31,79 +31,79 @@ class RTT:
         self.numWaypoints = 0
         self.Waypoints = []
 
-    def addChild(self, x, y, theta):
-        newNode = self.goal if (x, y, theta) == (self.goal.x, self.goal.y, self.goal.theta) else treeNode(x, y, theta)
+    def child_node(self, x, y, theta):
+        newNode = self.goal if (x, y, theta) == (self.goal.x, self.goal.y, self.goal.theta) else tree(x, y, theta)
         self.nearestNode.children.append(newNode)
         newNode.parent = self.nearestNode
         if newNode is not self.goal:
             self.currentNode = newNode
 
-    def samplePoint(self, goal):
+    def point_sample(self, goal):
         if np.random.rand() < 0.05:
             return np.array(goal)
         else:
             return np.random.uniform([0.1, 0.1, -pi], [1.9, 1.9, pi])
 
-    def goToPoint(self, start, end):
+    def search_point(self, start, end):
         offset = self.rho * (np.array(end) - np.array([start.x, start.y, start.theta])) / np.linalg.norm(
             np.array(end) - np.array([start.x, start.y, start.theta]))
         return np.array([start.x, start.y, start.theta]) + offset
 
-    def isInObstacle(self, start, end, obstacles):
+    def obstacle(self, start, end, obstacles):
         points = interpolate((start.x, start.y, start.theta), tuple(end))
         return any(not check_car(make_rigid_body((point[0], point[1]), point[2]), obstacles) for point in points)
 
-    def findNearest(self, root, point):
+    def nearest(self, root, point):
         if root:
-            dist = self.distance(root, point)
+            dist = self.space(root, point)
             if dist < self.nearestDist:
                 self.nearestNode, self.nearestDist = root, dist
             for child in root.children:
-                self.findNearest(child, point)
+                self.nearest(child, point)
 
-    def distance(self, node1, point):
+    def space(self, node1, point):
         linear_distance = sqrt((node1.x - point[0]) ** 2 + (node1.y - point[1]) ** 2)
         angular_distance = abs(change_angles(node1.theta) - change_angles(point[2]))
         return 0.7 * linear_distance + 0.3 * angular_distance
 
-    def goalFound(self, point, obstacles):
-        return self.distance(self.goal, point) <= self.rho and not self.isInObstacle(self.currentNode,
-                                                                                     [self.goal.x, self.goal.y,
+    def goal(self, point, obstacles):
+        return self.space(self.goal, point) <= self.rho and not self.obstacle(self.currentNode,
+                                                                              [self.goal.x, self.goal.y,
                                                                                       self.goal.theta], obstacles)
 
-    def resetNearestValues(self):
+    def reset(self):
         self.nearestNode = None
         self.nearestDist = float('inf')
 
-    def retracePath(self, goal):
+    def retrace(self, goal):
         if (goal.x, goal.y, goal.theta) != (self.randomTree.x, self.randomTree.y, self.randomTree.theta):
             self.numWaypoints += 1
             self.Waypoints.insert(0, [goal.x, goal.y, goal.theta])
             self.path_distance += self.rho
-            self.retracePath(goal.parent)
+            self.retrace(goal.parent)
 
 
-def rtt_tree(start, goal, obstacles):
+def calculate_tree(start, goal, obstacles):
     plt.close('all')
     ax = plt.figure("RTT Algorithm").gca()
     ax.set_xlim(0, 2)
     ax.set_ylim(0, 2)
     plt.plot(start[0], start[1], 'ro', alpha=0.1)
 
-    rrt = RTT(start, goal, 1000, .3)
+    rrt = rotate(start, goal, 1000, .3)
     for _ in range(1000):  # using for loop instead of while with manual increment
-        rrt.resetNearestValues()
-        point = rrt.samplePoint(goal)
-        rrt.findNearest(rrt.randomTree, point)
-        new = rrt.goToPoint(rrt.nearestNode, point)
-        if not rrt.isInObstacle(rrt.nearestNode, new, obstacles):
-            rrt.addChild(*new)
-            if rrt.goalFound(new, obstacles):
-                rrt.addChild(*goal)
+        rrt.reset()
+        point = rrt.point_sample(goal)
+        rrt.nearest(rrt.randomTree, point)
+        new = rrt.search_point(rrt.nearestNode, point)
+        if not rrt.obstacle(rrt.nearestNode, new, obstacles):
+            rrt.child_node(*new)
+            if rrt.goal(new, obstacles):
+                rrt.child_node(*goal)
                 print("Goal Found")
                 break
 
-    rrt.retracePath(rrt.goal)
+    rrt.retrace(rrt.goal)
     rrt.Waypoints.insert(0, start)
     for waypoint_start, waypoint_end in zip(rrt.Waypoints, rrt.Waypoints[1:]):
         plt.plot([waypoint_start[0], waypoint_end[0]], [waypoint_start[1], waypoint_end[1]], 'ro', linestyle="--")
@@ -112,16 +112,16 @@ def rtt_tree(start, goal, obstacles):
     plt.close()
     fig, ax = plt.subplots(dpi=100)
     rig_body = CarController(ax, car=make_rigid_body(start), obstacles=[])
-    rigid_graph(start, rig_body, rrt.Waypoints, obstacles)
+    graph(start, rig_body, rrt.Waypoints, obstacles)
 
 
-def rigid_graph(start, rig_body, waypoints, obstacles):
+def graph(start, rig_body, waypoints, obstacles):
     for waypoint in waypoints[1:]:
-        move_rigid(rig_body, start, waypoint, obstacles)
+        move_graph(rig_body, start, waypoint, obstacles)
         start = waypoint
 
 
-def move_rigid(rig_body, start, goal, obstacles):
+def move_graph(rig_body, start, goal, obstacles):
     for pt in interpolate(start, goal, 0.01):
         print(pt)
         reposition_car(pt, rig_body)
@@ -154,7 +154,7 @@ def main():
     goal = np.array([*args.goal[:2], change_angles(args.goal[2])])
     poly_map = load_polygons(args.map)
 
-    rtt_tree(start, goal, poly_map)
+    calculate_tree(start, goal, poly_map)
 
 
 if __name__ == '__main__':
